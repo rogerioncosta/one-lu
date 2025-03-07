068389ed77a124a12b6a271aefaf284cae58c16d
rogerioncosa <rogerioncosta1@gmail.com>
2025-03-07 17:35:16 -0300

    env. variables

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,14 +1,26 @@
+// require("dotenv").config();
+// const { Client } = require("pg");
+
+// const client = new Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// });
+
+// client.connect();
+
+// module.exports = { client };
+
 require("dotenv").config();
 const { Client } = require("pg");
 
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL, // Use variável de ambiente
+    ssl: { rejectUnauthorized: false } // Necessário para Neon
 });
 
-client.connect();
+// client.connect();
 
 module.exports = { client };
